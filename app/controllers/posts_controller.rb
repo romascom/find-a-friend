0629b5ecae246a52a0686dbd6985184b522b6895
Matthew Romasco <matthew@matthew-ThinkPad-T430s.(none)>
2014-01-21 16:36:25 -0800

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :text))
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+     @posts = Post.all
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'd94fb8811d94031ebfdc220ae1938447a272163b71bd0bed0b8650fec91e8752ad9a7cac2826de1d61a715ea6ccdde3ee1d1876d9c7edf127b01e91a3f5a5551'
