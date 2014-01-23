65e8124c262ba4a87ce3dc93aa62e9df93e99e40
romascom <matthew.romasco@gmail.com>
2014-01-23 11:28:12 -0800

    Got our first test to pass

---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+# spec/factories/posts.rb
+require 'faker'
+
+FactoryGirl.define do
+  factory :post do
+    title "THE BEST TITLE EVER"
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,11 @@
-# spec/models/post.rb
+# spec/models/post_spec.rb
 require 'spec_helper'
+require 'factory_girl_rails'
 
 describe Post do
+  it "has a valid factory" do
+    FactoryGirl.create(:post).should be_valid
+  end
+  it "is invalid without a title"
   it "is valid if it is at least 5 characters long"
 end
