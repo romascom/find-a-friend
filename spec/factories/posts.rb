21f111e694ba71f53ddfda334a630322dbb959d3
romascom <matthew.romasco@gmail.com>
2014-01-28 11:52:41 -0800

    added passing unit tests and css to style the list of posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,9 @@
 require 'faker'
 
 FactoryGirl.define do
-  factory :post do
-    title "THE BEST TITLE EVER"
+  factory :post do |f|
+    f.title { Faker::Name.first_name }
+    f.description { Faker::Name.last_name }
+    #title "THE BEST TITLE EVER"
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,6 +6,16 @@ describe Post do
   it "has a valid factory" do
     FactoryGirl.create(:post).should be_valid
   end
-  it "is invalid without a title"
-  it "is valid if it is at least 5 characters long"
+  it "is invalid without a title" do
+    FactoryGirl.build(:post, title: nil).should_not be_valid
+  end
+  it "is invalid if its title is less than 5 characters long" do
+    FactoryGirl.build(:post, title: "WHAT").should_not be_valid
+  end
+  it "is valid without a description" do
+    FactoryGirl.build(:post, description: nil).should be_valid
+  end
+  it "is valid if its title is at least 5 characters long" do
+    FactoryGirl.build(:post, title: "candy").should be_valid
+  end
 end
