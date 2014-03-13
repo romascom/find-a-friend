ba6b1acffaba8d76323796df0cffb3d4a2759f8a
romascom <matthew.romasco@gmail.com>
2014-03-13 10:56:04 -0700

    added some tests

--- spec/views/_form_spec.rb
+++ spec/views/_form_spec.rb
@@ -9,4 +9,10 @@ describe "posts/_form.html.erb" do
   it "renders a form" do
     response.should have_selector("form")
   end
+  
+  it "rejects dates of incorrect format"
+  
+  it "rejects times of incorrect format"
+  
+  it "renders a calendar view"
 end
