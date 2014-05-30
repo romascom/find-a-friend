da1f4ade75f19ea3c6d82d03a1c440110bf7ae23
romascom <matthew.romasco@gmail.com>
2014-05-29 18:40:55 -0700

    fixed email address validation such that blank addresses are accepted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true,
                     length: { minimum: 5 }
-  validates :recipients, presence: true, email: true
+  validates :recipients, email: true
 end
