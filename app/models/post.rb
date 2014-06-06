cfc645d6b3a73efd77cb9ef7e3f83b28bffefd76
romascom <matthew.romasco@gmail.com>
2014-06-05 17:54:59 -0700

    successfully merged joshua's code with mine

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true,
                     length: { minimum: 5 }
-  validates :recipients, email: true
+  belongs_to :user
+  validates :description, presence: true, length: {maximum: 255}
+  validates :user_id, presence: true
 end
