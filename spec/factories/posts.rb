de5f4b7922e6e38ca7a0aab4771ef5344a386b34
balesh2 <balesh@onid.oregonstate.edu>
2014-08-25 14:23:45 -0700

    adds more info to homepage and fixes post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,10 +1,14 @@
-# spec/factories/posts.rb
-require 'faker'
-
 FactoryGirl.define do
-  factory :post do |f|
-    f.title { "This is a title" }
-    f.description { Faker::Name.last_name }
-    #title "THE BEST TITLE EVER"
+  factory :posts do
+    sequence(:title) { |n| "Group Title Number #{n}" }
+    description "Description Text"
+    meeting_time "2014-07-02 13:58:43"
+    expire_time "3014-07-02 13:58:43"
+
+    trait :with_recipient do
+      after(:build) do |post|
+        post.recipients = "test@test.com"
+      end
+    end
   end
 end
