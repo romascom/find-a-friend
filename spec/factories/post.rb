ec03f68b326571edfcbe5a55e43ba183b00f3a3e
balesh2 <balesh@onid.oregonstate.edu>
2014-08-25 15:08:59 -0700

    trying to fix factory

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -4,6 +4,10 @@ FactoryGirl.define do
     description "Description Text"
     meeting_time "2014-07-02 13:58:43"
     expire_time "3014-07-02 13:58:43"
+    x 0
+    y 0
+    recipients nil
+    user_id 1
 
     trait :with_recipient do
       after(:build) do |post|
