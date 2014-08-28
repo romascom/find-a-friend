cc7046ba32abdf9aa2a1005be4aa1ebbb6039864
balesh2 <balesh@onid.oregonstate.edu>
2014-08-28 14:16:52 -0700

    maybe it'll work this time?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827225159) do
+ActiveRecord::Schema.define(version: 20140828162717) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -26,9 +26,6 @@ ActiveRecord::Schema.define(version: 20140827225159) do
     t.datetime "end_time"
     t.string   "location"
     t.string   "onid"
-  end
-
-  create_table "user", force: true do |t|
   end
 
 end
