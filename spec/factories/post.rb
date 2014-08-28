e502a016146d42ef3e28238f85bc792d528412e0
straleyb <straleyb@onid.oregonstate.edu>
2014-08-28 12:30:31 -0700

    made location required and errors work properly

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :post do
     sequence(:title) { |n| "Group Title Number #{n}" }
     description "Description Text"
+    location "Location Text"
     meeting_time "2014-07-02 13:58:43"
     expire_time "3014-07-02 13:58:43"
     x 0
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,5 +7,5 @@ describe Post do
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:description)}
   it {should validate_presence_of(:onid)}
-  
+  it {should validate_presence_of(:location)}
 end
