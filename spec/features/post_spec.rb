46b2e38a0ee6b06856b045089dafb2daaa8faaef
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-08-29 10:33:50 -0700

    added tests for mailer for new post and edited post

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,11 +4,11 @@ describe 'Posts' do
   let(:post) {create(:post)}
 
   context "When logged in" do
-	before do
+  	before do
       RubyCAS::Filter.fake("testonid")
       visit signin_path
       visit root_path
-  	end
+    end
 
     context "and trying to create a new post" do
   	  before do
@@ -18,60 +18,60 @@ describe 'Posts' do
       context "after filling all the forms out" do
         before do
           fill_in "Title", :with => "Test Title"
-    	  fill_in "Description", :with => "Test Description"
-    	  fill_in "Location", :with => "Test Location"
+    	    fill_in "Description", :with => "Test Description"
+    	    fill_in "Location", :with => "Test Location"
           fill_in "Meeting time", :with => Time.now
           fill_in "End time", :with => Time.now
-    	  click_button "Create Post"
+    	    click_button "Create Post"
         end
         it "should save and display it" do
-    	  expect(page).to have_content("Test Title")
-    	  expect(page).to have_content("Test Description")
-    	  expect(page).to have_content("Test Location")
+    	    expect(page).to have_content("Test Title")
+    	    expect(page).to have_content("Test Description")
+    	    expect(page).to have_content("Test Location")
         end
         context "When trying to edit a post" do
-    	  before do
-    	    click_link "Edit"
-    	  end
-    	  context "should let you fill out new information" do
-    	    before do
-    		  fill_in "Description", :with => "New Description"
-    		  click_button "Update Post"
-    	    end
-    	    it "should display the new information" do
-    		  expect(page).to have_content("New Description") 
-    	    end
-    	  end
+      	  before do
+      	    click_link "Edit"
+      	  end
+      	  context "should let you fill out new information" do
+      	    before do
+      		    fill_in "Description", :with => "New Description"
+      		    click_button "Update Post"
+      	    end
+      	    it "should display the new information" do
+      		    expect(page).to have_content("New Description") 
+      	    end
+      	  end
         end
         context "When trying to delete the post" do
-    	  before do
-    	    click_link "Delete"
-    	  end
-    	  it "should be deleted" do
-    	    expect(Post.count).to eq 0
-    	  end
+      	  before do
+      	    click_link "Delete"
+      	  end
+      	  it "should be deleted" do
+      	    expect(Post.count).to eq 0
+      	  end
         end
       end
     end
   end
   context "When not logged in" do
     context "and trying to create a new post" do
-	  before do
-		visit root_path
-		click_link "New post"
-	  end
-	  it "should give you an insufficient permissions error" do
-		expect(page).to have_content(I18n.t("permission_error.error_string"))
-	  end
+	    before do
+		    visit root_path
+		    click_link "New post"
+	    end
+	    it "should give you an insufficient permissions error" do
+		    expect(page).to have_content(I18n.t("permission_error.error_string"))
+	    end
     end
-	context "when trying to edit a post" do
-	  before do
-		visit edit_post_path(post.id)
+	  context "when trying to edit a post" do
+  	  before do
+  		  visit edit_post_path(post.id)
+  	  end
+	    it "should give you an insufficient permissions error" do
+		    expect(page).to have_content(I18n.t("permission_error.error_string"))
+	    end
 	  end
-	  it "should give you an insufficient permissions error" do
-		expect(page).to have_content(I18n.t("permission_error.error_string"))
-	  end
-	end
     context "when visiting the show view" do
       before do
         visit post_path(post)
